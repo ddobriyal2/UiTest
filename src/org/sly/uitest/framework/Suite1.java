04b96d6b8356943349a5aa5c3900efe747ecb179
DeveshDobriyal <dobriyaldevesh@gmail.com>
2018-02-02 00:03:17 +0800

    Added Suite1

--- src/org/sly/uitest/framework/Suite1.java
+++ src/org/sly/uitest/framework/Suite1.java
@@ -1,5 +1,15 @@
 package org.sly.uitest.framework;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.sly.uitest.sections.basesystem.BaseSystemTest;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ BaseSystemTest.class
+})
 public class Suite1 {
 
 }
